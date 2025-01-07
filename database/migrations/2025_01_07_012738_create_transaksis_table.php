be55723f283663d9bbdc474a5888ced32b1502d9
dindaputriaprlia <putriapriliadinda26@gmail.com>
2025-01-07 14:19:20 +0700

    first commit

---
+++ database/migrations/2025_01_07_012738_create_transaksis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_barang');
+            $table->integer('id_kasir');
+            $table->integer('id_pelanggan');
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
